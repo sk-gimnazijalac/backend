0c2a2cff310c09287e6b60bcdf7a961e7c8425f3
fvasicc <filip.vasic2000@gmail.com>
2024-07-03 20:38:34 +0200

    feat: add images to the database
    Enabled saving images as blobs in the database. When creating a post, the image is saved in the database. Added an endpoint to retrieve an image in base64 format based on the image identifier.
    
    Author: fvasicc

--- skGimnazijalac/sk_gimnazijalac_posts/serializers.py
+++ skGimnazijalac/sk_gimnazijalac_posts/serializers.py
@@ -1,6 +1,8 @@
+import base64
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Image
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -18,11 +20,16 @@ class GetPostSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=100, required=True)
     author = AuthorSerializer()
     date = serializers.DateField(required=True)
-    src = serializers.CharField(max_length=100)
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
+
+    def get_image(self, obj):
+        if obj.image:
+            return '/post/image/' + str(obj.image.id)
+        return None
 
 
 class CreatePostSerializer(serializers.ModelSerializer):
@@ -31,20 +38,43 @@ class CreatePostSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=100, required=True)
     author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     date = serializers.DateField(required=True)
-    src = serializers.CharField(max_length=100)
+    image = serializers.ImageField(required=True)
 
     class Meta:
         model = Post
         fields = '__all__'
 
     def create(self, validated_data):
+        image = validated_data.pop('image')
+        image_bytes = image.read()
+        image_instance = Image.objects.create(content=image_bytes)
+        validated_data['image'] = image_instance
         return Post.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.description = validated_data.get('description', instance.description)
         instance.shortDescription = validated_data.get('shortDescription', instance.shortDescription)
         instance.title = validated_data.get('title', instance.title)
-        instance.src = validated_data.get('src', instance.src)
-
+        if 'image' in validated_data:
+            image = validated_data.pop('image')
+            image_bytes = image.read()
+            image_instance = Image.objects.create(content=image_bytes)
+            instance.image = image_instance.id
         instance.save()
         return instance
+
+
+class Base64ImageField(serializers.Field):
+    def to_representation(self, obj):
+        return base64.b64encode(obj).decode('utf-8')
+
+    def to_internal_value(self, data):
+        pass
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    content = Base64ImageField()
+
+    class Meta:
+        model = Image
+        fields = ('content',)
